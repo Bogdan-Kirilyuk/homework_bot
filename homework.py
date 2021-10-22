bfc88176e7975a7d20a8f023c6f992f4a2bd6c85
Bogdan-Kirilyuk <kirilyuk.surgut@gmail.com>
2021-10-22 13:05:05 +0500

    тест бота на хероку

--- homework.py
+++ homework.py
@@ -13,7 +13,7 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
-RETRY_TIME = 6
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
